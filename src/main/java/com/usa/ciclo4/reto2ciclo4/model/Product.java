714b7196aec7a2eb333beb241e87f490a900a1b5
Miguel Bayter <mbayterq.dev@gmail.com>
2021-12-12 17:37:24 -0500

    reto2Back

---
+++ src/main/java/com/usa/ciclo4/reto2ciclo4/model/Product.java
@@ -0,0 +1,23 @@
+package com.usa.ciclo4.reto2ciclo4.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "products")
+public class Product {
+    @Id
+    private String reference;
+    private String brand;
+    private String category;
+    private String description;
+    private double price;
+    private int quantity;
+    private String photography;
+    private boolean availability = true;
+}
